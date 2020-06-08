2a5b3413ae934b2b5b690e20367e3dca1a96d350
Ikhlas Ismi <ikhlas4527@gmail.com>
2020-06-08 16:42:34 +0530

    Added Section 1 and Navbar Modified

--- Client/js/function.js
+++ Client/js/function.js
@@ -1,18 +1,19 @@
+//Hamburger Setup
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click',()=>{
-        
+    burger.addEventListener('click', () => {
+
         //Toggle Nav
         nav.classList.toggle('nav-active');
 
         //Animate Links
-        navLinks.forEach((link,index)=>{
-            if(link.style.animation){
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
                 link.style.animation = ''
-            }else{
+            } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
@@ -23,3 +24,10 @@ const navSlide = () => {
 }
 
 navSlide();
+
+//Creativity Text Animation
+const creativity = document.querySelectorAll("#creativity path");
+
+for (let i = 0; i < creativity.length; i++) {
+    console.log(`Letter ${i} is ${creativity[i].getTotalLength()}`);
+} 
