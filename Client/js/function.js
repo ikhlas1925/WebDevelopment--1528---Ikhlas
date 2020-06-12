47d2ae045e92e5b8b04eea1b510469e9e19bef4d
Ikhlas Ismi <ikhlas4527@gmail.com>
2020-06-12 12:27:58 +0530

    Testimonials.html (WIP)

--- Client/js/function.js
+++ Client/js/function.js
@@ -48,3 +48,52 @@ $(document).ready(function() {
 
     });
 });
+
+//Testimonials
+var featureSubmitBtn = document.getElementById("featureSubmitBtn");
+var featureFrm = document.getElementById("featureFrm");
+var featureLoadingDiv = document.getElementById("featureLoadingDiv");
+
+featureSubmitBtn.addEventListener("click", async function() {
+  // Make the loader div visible
+  featureLoadingDiv.classList.remove("human-removed");
+  // Hide the form from the user
+  featureFrm.classList.add("human-removed");
+  await onfeaturesubmit();
+  featureFrm.reset();
+  featureLoadingDiv.classList.add("human-removed");
+  featureFrm.classList.remove("human-removed");
+  await onfeatureload()
+
+});
+
+async function onfeatureload() {
+  var response = await fetch("http://localhost:3000/features");
+  var result = await response.json();
+
+  var featureList = document.getElementById("feature-list")
+  featureList.innerHTML = "";
+
+  for (var i = 0; i < result.length; i++){
+    var lielement = document.createElement("li");
+    lielement.className = "list-group-item";
+    lielement.innerHTML =
+    result[i].body + "<span class='badge badge-success'>" + result[i].author + "," + result[i].time + "</span>";
+    featureList.appendChild(lielement);
+  }
+
+}
+onfeatureload();
+
+async function onfeaturesubmit(){
+  await fetch("http://localhost:3000/features", {
+    method: "post",
+    body: JSON.stringify({
+      name: document.getElementById("username").value,
+      feature: document.getElementById("userfeature").value
+    }),
+    headers: {
+      "content-type": "application/json"
+    }
+  });
+}
